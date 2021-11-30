41f4f8b21215a1bfbd135fa277ae1a61c8b4acc6
Rafael Goncalves <capitao.rafa@gmail.com>
2021-11-30 20:43:37 -0300

    Sample code

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 import puppeteer from 'puppeteer';
+(async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://anycart.com/');
+    await page.screenshot({
+        path: 'example.png'
+    });
 
-
-console.log('hi', puppeteer)
+    await browser.close();
+})();
