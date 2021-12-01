3be10d06a2ac10e5916aaf2a35492272d91ab840
Rafael Goncalves <capitao.rafa@gmail.com>
2021-11-30 21:51:56 -0300

    Scrolling to the bottom of the page

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
 import puppeteer from 'puppeteer';
 
+//todo: move these safeFunctions to decorators, same with logging and delay functions
 async function safeClick(page, selector) {
-    await page.waitForSelector(selector) //todo: handle timeout
-    await page.click(selector)
+    try {
+        await page.click(selector)
+    } catch (error) {
+        await page.waitForSelector(selector) //todo: handle timeout instead of try catch
+        await page.click(selector)
+    }
     return
+}
+async function safeType(page, selector, message) {
+    try {
+        await page.type(selector, message)
+    } catch (error) {
+        await page.waitForSelector(selector)
+        await page.type(selector, message)
+    }
+    return
+}
+
+async function scrollBottom(page) {
+    await page.evaluate(async () => {
+        let scrollPosition = 0
+        let documentHeight = document.body.scrollHeight
+
+        while (documentHeight > scrollPosition) {
+            window.scrollBy(0, documentHeight)
+            await new Promise(resolve => {
+                setTimeout(resolve, 1000)
+            })
+            scrollPosition = documentHeight
+            documentHeight = document.body.scrollHeight
+        }
+    })
 }
 
 (async () => {
@@ -17,18 +47,24 @@ async function safeClick(page, selector) {
     })
 
     await page.goto('https://anycart.com/');
-
     await safeClick(page, '[data-testid="modal-close"]')
-    // await page.waitForTimeout(5000);
-    // page.waitForSelector('[data-testid="modal-close"]', {
-    //     visible: true
-    // })
+    await safeClick(page, '[class="artemis-btn --a"]') //could have used /select-shop path and skipped the steps above
+    await safeType(page, '[data-testid="address-l1-autocomplete"]', '94306')
+    await safeClick(page, '[data-testid="address-list-item"]')
+    await safeClick(page, '[data-testid="select-shop-partner"]')
 
-    // const el = await page.$('document.querySelector("body > div.home-template.app-has-sidebar.app--has-header.app-has-sidebar--hamburger-menu-is-closed.app-has-sidebar--is-open > section > div > div > div.app-promo-onboarding-modal.generic-modal > div > span > div > div.right-section > div > button")')
-    // await page.click('document.querySelector("body > div.home-template.app-has-sidebar.app--has-header.app-has-sidebar--hamburger-menu-is-closed.app-has-sidebar--is-open > section > div > div > div.app-promo-onboarding-modal.generic-modal > div > span > div > div.right-section > div > button")')
-    // await page.screenshot({
-    //     path: 'example.png'
-    // });
+    await page.evaluate(() => {
+        window.scrollTo(0, window.document.body.scrollHeight)
+    });
 
-    // await browser.close();
+    await scrollBottom(page)
+
+    const children = await page.$$('[id="gallery-13"]');
+
+    console.log("Result:", children);
+    // await safeClick(page, '[id="gallery-13"]')
+
+    // id="gallery-13"
+
+    // qty-btn __add __initial
 })();
