5e6d80f80a7f6fa3ccdd9d4f58e5714655fafa1c
Rafael Goncalves <capitao.rafa@gmail.com>
2021-11-30 21:04:57 -0300

    Button press

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 import puppeteer from 'puppeteer';
-(async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto('https://anycart.com/');
-    await page.screenshot({
-        path: 'example.png'
-    });
 
-    await browser.close();
+(async () => {
+    const browser = await puppeteer.launch({
+        headless: false
+    });
+    const page = await browser.newPage();
+
+
+    await page.goto('https://anycart.com/');
+
+    await page.waitForTimeout(5000);
+
+    await page.click('[data-testid="modal-close"]')
+
+
+    // const el = await page.$('document.querySelector("body > div.home-template.app-has-sidebar.app--has-header.app-has-sidebar--hamburger-menu-is-closed.app-has-sidebar--is-open > section > div > div > div.app-promo-onboarding-modal.generic-modal > div > span > div > div.right-section > div > button")')
+    // await page.click('document.querySelector("body > div.home-template.app-has-sidebar.app--has-header.app-has-sidebar--hamburger-menu-is-closed.app-has-sidebar--is-open > section > div > div > div.app-promo-onboarding-modal.generic-modal > div > span > div > div.right-section > div > button")')
+    // await page.screenshot({
+    //     path: 'example.png'
+    // });
+
+    // await browser.close();
 })();
