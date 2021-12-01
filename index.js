38b3d7b8e3bf483b3bfde380ed3113f10c719a6f
Rafael Goncalves <capitao.rafa@gmail.com>
2021-12-01 11:02:13 -0300

    Multiple clicks

--- index.js
+++ index.js
@@ -2,13 +2,20 @@ import puppeteer from 'puppeteer';
 
 //todo: move these safeFunctions to decorators, same with logging and delay functions
 async function safeClick(page, selector) {
-    try {
-        await page.click(selector)
-    } catch (error) {
-        await page.waitForSelector(selector) //todo: handle timeout instead of try catch
-        await page.click(selector)
-    }
+    console.log('safeClick', selector);
+    await page.waitForSelector(selector) //todo: handle timeout instead of try catch
+    console.log('Found')
+    await page.click(selector)
+    console.log('Clicked', selector)
     return
+}
+// cant reuse safeClick because of context change
+async function safeClickMultiple(page, selector) { //todo: actually be 'safe'
+    console.log('safeClickMultiple', selector);
+    await page.$$eval(selector, (links) => {
+        links.forEach(link => link.click())
+    })
+    console.log('Clicked Multiple')
 }
 async function safeType(page, selector, message) {
     try {
@@ -48,19 +55,28 @@ async function scrollBottom(page) {
 
     await page.goto('https://anycart.com/');
     await safeClick(page, '[data-testid="modal-close"]')
-    await safeClick(page, '[class="artemis-btn --a"]') //could have used /select-shop path and skipped the steps above
+    await safeClick(page, '[href="/select-shop"]') //could have used /select-shop path and skipped the steps above
     await safeType(page, '[data-testid="address-l1-autocomplete"]', '94306')
     await safeClick(page, '[data-testid="address-list-item"]')
     await safeClick(page, '[data-testid="select-shop-partner"]')
 
 
     await scrollBottom(page)
+    //todo: add failsafe when less than $30
+    await safeClickMultiple(page, 'li.app-craft-item-interactive .qty-btn.__add');
 
-    const recipes = await page.$$('li.app-craft-item-interactive');
-    // id="gallery-xx" is not static
-    // app-craft-item-interactive is unique to recipes
-    recipes.forEach(async (recipe) => {
-        const btn = await recipe.$('[class="qty-btn __add __initial"]')
-        await btn.click()
-    });
+    await page.waitForTimeout(1000);
+    await safeClick(page, '[class="top-nav-item cart-item __has-items"]')
+
+
+    await safeClick(page, 'button.ch-o-btn')
+
+
+    // todo: check if 'staples' modal actually opens
+    // todo: find a better indentifier
+    await safeClick(page, '[class="anycart-btn btn-med btn-branding-color btn-font-normal"]')
+    // class="place-order-button anycart-btn btn-branding-color disabled btn-font-big btn-med"
+    //class="payment-button-container place-order-button anycart-btn btn-branding-color btn-font-big-bold btn-med"
+
+
 })();
